e969cdb6a74007660570c9aee116641c569e5406
ErenYeager2025 <tongxiangyu3@gmail.com>
2025-04-23 16:07:40 +0100

    Added Firebase register functionality

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, setDoc } from 'firebase/firestore';
-import { auth, db } from '../firebase';
+import { auth, db } from './firebaseConfig';
 
 function Register() {
   const [email, setEmail] = useState('');
